4d7fdf025219bf009bcafde23bae625261f632b5
Phillip Gibson <phillipgibson@fastmail.com>
2022-08-17 15:30:05 -0400

    better null checking

--- scripts/add-post-to-index.sh
+++ scripts/add-post-to-index.sh
@@ -1,4 +1,15 @@
 title=$(echo $1 | sed -e 's/-\([a-z]\)/ \U\1/g' -e 's/^\([a-z]\)/\U\1/')
-l=$2
-d=$3
-sed -i "${l=3}i \| ${d=$(date +"%a %b %d %Y")} \| \[$title\](/posts/$1) \|" raw/index.md
+
+if [ -z ${2+x} ]; then
+  l=3
+else
+  l=$2
+fi
+
+if [ -z ${3+x} ]; then
+  d=$(date +"%a %b %d %Y")
+else
+  d=$3
+fi
+
+sed -i "${l}i \| $d \| \[$title\](/posts/$1) \|" raw/index.md
