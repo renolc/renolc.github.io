b7586497379f04a43279b301b60770c5fd322bcb
Phillip Gibson <phillipgibson@fastmail.com>
2022-08-17 10:39:46 -0400

    publics -> docs

--- scripts/compile-page.sh
+++ scripts/compile-page.sh
@@ -1,3 +1,3 @@
 file_name=$(basename $1 '.md')
 base_path=$(dirname $1)
-pandoc $1 --template templates/html.template -f gfm+hard_line_breaks -s -o public${base_path:3}/$file_name.html
+pandoc $1 --template templates/html.template -f gfm+hard_line_breaks -s -o docs${base_path:3}/$file_name.html
