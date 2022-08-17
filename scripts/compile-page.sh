b72ce97a7bb28c624c81ef2c90527b574da5a044
Phillip Gibson <phillipgibson@fastmail.com>
2022-08-17 10:20:31 -0400

    compile script logging

--- scripts/compile-page.sh
+++ scripts/compile-page.sh
@@ -1,3 +1,6 @@
 file_name=$(basename $1 '.md')
 base_path=$(dirname $1)
+echo "file_name $file_name"
+echo "base_path $base_path"
+echo "base_path:3 ${base_path:3}"
 pandoc $1 --template ./templates/html.template -f gfm+hard_line_breaks -s -o ./public${base_path:3}/$file_name.html
