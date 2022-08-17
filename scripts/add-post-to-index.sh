5c14c11435127857e6900749506029d95664bdc0
Phillip Gibson <phillipgibson@fastmail.com>
2022-08-17 15:21:06 -0400

    clear

--- scripts/add-post-to-index.sh
+++ scripts/add-post-to-index.sh
@@ -1,4 +1,4 @@
 title=$(echo $1 | sed -e 's/-\([a-z]\)/ \U\1/g' -e 's/^\([a-z]\)/\U\1/')
-$l="$2"
-$d="$3"
+l=$2
+d=$3
 sed -i "${l=3}i \| ${d=$(date +"%a %b %d %Y")} \| \[$title\](/posts/$1) \|" raw/index.md
