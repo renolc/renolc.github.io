a6de101e42ac28bd52ef5722062a045314ffa9be
Phillip Gibson <phillipgibson@fastmail.com>
2022-08-17 08:37:01 -0400

    initial commit, setup workflow testing

---
+++ scripts/compile-all.sh
@@ -0,0 +1,1 @@
+find raw -type f -name "*.md" | xargs -n 1 sh scripts/compile-page.sh
---
+++ scripts/compile-page.sh
@@ -0,0 +1,3 @@
+file_name=$(basename $1 '.md')
+base_path=$(dirname $1)
+pandoc $1 --template templates/html.template -f gfm+hard_line_breaks -s -o public${base_path:3}/$file_name.html
