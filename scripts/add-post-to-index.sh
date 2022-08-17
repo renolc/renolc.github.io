43d5dccbec58febf84983604f63b4c03a4debd61
Phillip Gibson <phillipgibson@fastmail.com>
2022-08-17 14:36:37 -0400

    try fix

--- scripts/add-post-to-index.sh
+++ scripts/add-post-to-index.sh
@@ -1,2 +1,3 @@
 title=$(echo $1 | sed -e 's/-\([a-z]\)/ \U\1/g' -e 's/^\([a-z]\)/\U\1/')
-sed -i "${2=3}i \| date \| \[$title\](/posts/$1) \|" raw/index.md
+$l="$2"
+sed -i "${l=3}i \| date \| \[$title\](/posts/$1) \|" raw/index.md
