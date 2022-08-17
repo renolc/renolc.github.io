2afac903b3bf0abc4345d201015515aec0184f3d
Phillip Gibson <phillipgibson@fastmail.com>
2022-08-17 10:32:17 -0400

    add htmls

--- scripts/compile-page.sh
+++ scripts/compile-page.sh
@@ -3,4 +3,4 @@ base_path=$(dirname $1)
 echo "file_name $file_name"
 echo "base_path $base_path"
 echo "base_path:3 ${base_path:3}"
-pandoc $1 --template ./templates/html.template -f gfm+hard_line_breaks -s -o ./public${base_path:3}/$file_name.html
+pandoc $1 --template templates/html.template -f gfm+hard_line_breaks -s -o public${base_path:3}/$file_name.html
--- scripts/delete-page.sh
+++ scripts/delete-page.sh
@@ -1,3 +1,3 @@
 file_name=$(basename $1 '.md')
 base_path=$(dirname $1)
-rm ./public${base_path:3}/$file_name.html
+rm public${base_path:3}/$file_name.html
