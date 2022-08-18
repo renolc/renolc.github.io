f3a55bc610525cc498bc1c1ca1b509c39ca4002f
Phillip Gibson <phillipgibson@fastmail.com>
2022-08-18 12:49:26 -0400

    simplify template, fix compile-all

--- scripts/compile-all.sh
+++ scripts/compile-all.sh
@@ -1,1 +1,5 @@
-find raw -type f -name "*.md" | xargs -n 1 sh scripts/compile-page.sh
+find raw -type f -name "*.md" | while read file; do
+  file_name=$(basename $file '.md')
+  base_path=$(dirname $file)
+  bash scripts/compile-page.sh $base_path $file_name
+done
